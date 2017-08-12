1f2c7c88a2234c30140038e9ce484debfc82602f
srikanthpragada <srikanthpragada@gmail.com>
2017-08-12 19:00:53 +0530

    12-AUG

---
+++ src/servlets/HelloServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  response.setContentType("text/html");
+		  PrintWriter pw = response.getWriter();
+		  String name = request.getParameter("name");
+		  if(name == null)
+			   name = "World!";
+		  
+		  pw.print("<h1>Hello " + name +  "</h1>");
+		  pw.print("<hr>");
+		  pw.print("<h3>" + new Date()  + "</h3>");
+			
+	}
+
+}
