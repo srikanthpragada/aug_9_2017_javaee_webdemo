452343dfb1828f6a740463c8267c05625880d463
srikanthpragada <srikanthpragada@gmail.com>
2017-09-13 17:58:17 +0530

    13-SEP

--- src/jaxws/JobsService.java
+++ src/jaxws/JobsService.java
@@ -3,17 +3,45 @@ package jaxws;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import oracle.jdbc.rowset.OracleCachedRowSet;
+
 @WebService
 public class JobsService {
 
+	//  0 - Success, 1 - Id not found, 2 - Updation failed 
 	@WebMethod
-	public void updateJob(String id, String newTitle) {
+	public int  updateJob(String id, String newTitle) {
 		
+        return 0;
 	}
 	
 	@WebMethod
 	public Job getJob(String id) {
-		
-		return null; 
+		try(OracleCachedRowSet rs = new OracleCachedRowSet()) {
+			rs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+			rs.setUsername("hr");
+			rs.setPassword("hr");
+
+			rs.setCommand("select * from jobs where upper(job_id) = ?");
+			rs.setString(1, id.toUpperCase());
+			rs.execute();
+			
+			if ( rs.next() )
+			{
+				Job job = new Job();
+				job.setId(rs.getString("job_id"));
+				job.setTitle(rs.getString("job_title"));
+				job.setMinSal(rs.getInt("min_salary"));
+				job.setMaxSal(rs.getInt("max_salary"));
+				return job; 
+			}
+			else
+				return null;
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+			return null;
+		}
+		 
 	}
 }
