a01a78fd60b84aaf6326f020b91d9e94ba27b218
srikanthpragada <srikanthpragada@gmail.com>
2017-09-18 17:58:57 +0530

    18-SEP

--- src/rest/EmpService.java
+++ src/rest/EmpService.java
@@ -1,10 +1,18 @@
 package rest;
+import java.sql.SQLException;
+
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
 import oracle.jdbc.rowset.OracleCachedRowSet;
 
@@ -43,9 +51,41 @@ public class EmpService {
 		}
 		
 	}
+	 
+	@GET 
+	@Path("/{id}")
+	public String getEmployee( @PathParam("id") String id) {
+		try(OracleCachedRowSet rs = new OracleCachedRowSet()) {
+			rs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+			rs.setUsername("hr");
+			rs.setPassword("hr");
+
+			rs.setCommand("select employee_id, first_name, last_name, salary, to_char(hire_date,'dd-MON-yyyy') hire_date from employees where employee_id = ?");
+			rs.setString(1, id);
+			rs.execute();
+			
+
+			if (rs.next()) {
+				JsonObjectBuilder builder = Json.createObjectBuilder();
+				builder.add("id", rs.getString("employee_id"));
+				builder.add("name", rs.getString("first_name") +  " " + rs.getString("last_name"));
+				builder.add("salary", rs.getString("salary"));
+				builder.add("hiredate", rs.getString("hire_date"));
+				return builder.build().toString();
+			}
+			else
+				throw new NotFoundException();
+		}
+		catch(SQLException ex) {
+			System.out.println(ex);
+			throw new InternalServerErrorException();
+		}
+	}
+
+
 	
 	@GET 
-	@Path("/{dept}")
+	@Path("/dept/{dept}")
 	public String getEmployeesByDept( @PathParam("dept") String dept) {
 		try(OracleCachedRowSet rs = new OracleCachedRowSet()) {
 			rs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
@@ -78,4 +118,34 @@ public class EmpService {
 		}
 	}
 
+	
+	@PUT 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Path("/{id}")
+	public void updateSalary(@PathParam("id") String id,
+			 @FormParam("newSalary") int newSalary) {
+		System.out.println(id);
+		System.out.println(newSalary);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
